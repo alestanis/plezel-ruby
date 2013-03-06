1d6405c20225273a271fb57fdf89cc471ffb443b
alejandra <ale.estanislao@gmail.com>
2013-03-07 00:50:32 +0100

    Added all the stub requests.

--- spec/plezel-ruby/check_spec.rb
+++ spec/plezel-ruby/check_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe "check" do
-  before :each do
-    Plezel.api_key = 'developer_api_key'
-    @res = Plezel.check('blocked_card_number', 2000, 'EUR', 'developer_api_key')
+
+  context "a blocked card" do
+    before :each do
+      Plezel.api_key = 'developer_api_key'
+      @res = Plezel.check('locked_card_number', 2000, 'EUR')
+    end
+
+    it "sends the right request" do
+      WebMock.should have_requested(:post, /.*\/card\/check.*/)
+        .with(body: "card=locked_card_number&amount=2000&currency=EUR")
+    end
+
+    it "response is not empty" do
+      @res.should_not be_nil
+    end
   end
 
-  it "sends the right request" do
-    WebMock.should have_requested(:post, /.*\/card\/check.*/)
-      .with(body: "card=blocked_card_number&amount=2000&currency=EUR")
-  end
-
-  it "response is not empty" do
-    @res.should_not be_nil
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,28 +6,228 @@ RSpec.configure do |config|
     ### Stub requests to avoid hitting the API
 
     ## /check
-    # Blocked card
-    stub_http_request(:post, /.*check.*/)
+    # Non-existent card
+    stub_http_request(:post, /.*developer_api_key.*check.*/)
       .with(
-      :body => /card=blocked_card_number/)
+      :body => /card=nonexistent_card_number/)
       .to_return({
         body: {
-          "status" => "not_found",
-          "url" => "http://api.lvh.me:3000/v1/card/check",
-          "data" => {
+          status: "not_found",
+          url: "#{Plezel.api_url}/v1/card/check",
+          data: {
           }
         }.to_json,
         status: 404
       })
-    # stub_request(:any, /.*/).to_return({
-    #   body: {
-    #     test: "Hello!"
-    #   },
-    #   status: 200
-    # })
+
+    # Locked card
+    stub_http_request(:post, /.*developer_api_key.*check.*/)
+      .with(
+      :body => /card=locked_card_number/)
+      .to_return({
+        body: {
+          status: "forbidden",
+          url: "#{Plezel.api_url}/v1/card/check",
+          data: {
+            card_status: "locked"
+          }
+        }.to_json,
+        status: 403
+      })
+
+    # Unlocked card
+    stub_http_request(:post, /.*developer_api_key.*check.*/)
+      .with(
+      :body => /card=unlocked_card_number/)
+      .to_return({
+        body: {
+          status: "ok",
+          url: "#{Plezel.api_url}/v1/card/check",
+          data: {
+            card_status: "unlocked"
+          }
+        }.to_json,
+        status: 200
+      })
+
+    # Validation card
+    stub_http_request(:post, /.*developer_api_key.*check.*/)
+      .with(
+      :body => /card=validation_card_number/)
+      .to_return({
+        body: {
+          status: "ok",
+          url: "#{Plezel.api_url}/v1/card/check",
+          data: {
+            card_status: "validation",
+            grant: {
+              amount: 2000,
+              created_at: "2013-03-06T23:21:49Z",
+              currency: "EUR",
+              merchant_id: "stub_merchant_id",
+              token: "stub_grant_token",
+              trials: 0,
+              validated: false,
+              validations: [
+                {
+                  type: "EmailValidator",
+                  label: "Code in validation e-mail"
+                },
+                {
+                  type: "GoogleOathValidator",
+                  label: "Google Authenticator Code"
+                },
+                {
+                  type: "IpValidator",
+                  label: "User's IP address"
+                },
+                {
+                  type: "QuestionValidator",
+                  label: "The answer"
+                },
+                {
+                  type: "SmsValidator",
+                  label: "Code in validation SMS"
+                }
+              ]
+            }
+          }
+        }.to_json,
+        status: 200
+      })
 
     ## /process
-    # 
-    # stub_http_request(:post, /.*process.*/).to_response
+    # Unexistent grant
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=unknown_grant_token/)
+      .to_return({
+        body: {
+          status: "not_found",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: {
+            grant: nil
+          },
+          error: {
+            message: "Grant not found.",
+            type: "not_found"
+          }
+        }.to_json,
+        status: 404
+      })
+
+    # Wrong answers
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=stub_grant_token_wrong/)
+      .to_return({
+        body: {
+          status: "forbidden",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: {
+            grant: {
+              amount: 2000,
+              created_at: "2013-03-06T23:32:51Z",
+              currency: "EUR",
+              merchant_id: "stub_merchant_id",
+              token: "stub_grant_token",
+              trials: 1,
+              validated: false,
+              errors: [
+                "Wrong email validation code.",
+                "Wrong Google Authenticator code.",
+                "Transactions are not allowed from this IP.",
+                "Wrong answer to the secret question.",
+                "Wrong SMS validation code."
+              ]
+            }
+          }
+        }.to_json,
+        status: 403
+      })
+
+    # Valid answers
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=stub_grant_token_right/)
+      .to_return({
+        body: {
+          status: "ok",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: {
+            grant: {
+              amount: 2000,
+              created_at: "2013-03-06T23:39:32Z",
+              currency: "EUR",
+              merchant_id: "stub_merchant_id",
+              token: "stub_grant_token",
+              trials: 1,
+              validated: true
+            }
+          }
+        }.to_json,
+        status: 200
+      })
+
+    # Already validated answers
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=stub_grant_token_already_validated/)
+      .to_return({
+        body: {
+          status: "unprocessable_entity",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: nil,
+          error: {
+            message: "This grant has already been validated.",
+            type: "unprocessable_entity"
+          }
+        }.to_json,
+        status: 422
+      })
+    
+    # Expired grant
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=stub_grant_token_expired/)
+      .to_return({
+        body: {
+          status: "forbidden",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: {
+            grant: {
+              amount: 2000,
+              created_at: "2013-03-06T23:35:20Z",
+              currency: "EUR",
+              merchant_id: "stub_merchant_id",
+              token: "stub_grant_token",
+              trials: 1,
+              validated: false
+            }
+          },
+          error: {
+            message: "The grant has expired. Please request a new grant.",
+            type: "forbidden"
+          }
+        }.to_json,
+        status: 403
+      }) 
+
+    # Too many trials
+    stub_http_request(:post, /.*developer_api_key.*process.*/)
+      .with(
+      :body => /token=stub_grant_token_too_many_trials/)
+      .to_return({
+        body: {
+          status: "unprocessable_entity",
+          url: "#{Plezel.api_url}/v1/card/process",
+          data: nil,
+          error: {
+            message: "This grant was processed too many times. For security reasons, it has been deactivated.",
+            type: "unprocessable_entity"
+          }
+        }.to_json,
+        status: 422
+      }) 
   end
 end
