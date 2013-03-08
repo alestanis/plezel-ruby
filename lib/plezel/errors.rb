25c34e4aa9afd15e4d74f939db556e9c6668c0bf
alejandra <ale.estanislao@gmail.com>
2013-03-08 22:49:08 +0100

    Added a to_s method to errors.

--- lib/plezel/errors.rb
+++ lib/plezel/errors.rb
@@ -2,13 +2,16 @@ module Plezel
   # Base error
   # The user can catch 'Plezel::Error' to get all exceptions coming from Plezel
   class Error < StandardError
+    def to_s
+      message
+    end
   end
 
   class AuthenticationError < Error
     def message
       "#{http_code_to_name(http_code)}: HTTP Access Denied.
       Did you forget to set your api key? This can be done with
-      
+
           Plezel.api_key = <your_api_key>
 
       If your api key does not work, please contact Plezel."
